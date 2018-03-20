2006f5b407ecfc81170b3f71492e0512b924109a
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2018-03-20 13:03:53 -0300

    redux

---
+++ simple-redux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    {"title": "blablabla blablabla"},
+    {"title": "blablabla blobloblo"},
+    {"title": "blablabla blebleble"},
+    {"title": "blablabla bliblibli"}
+  ]
+}
